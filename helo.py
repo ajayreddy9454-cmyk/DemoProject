17ab0dafd0c4569181d3ce567c970ffe6d86e8e1
ajayreddy9454-cmyk <ajayreddy9454@gmail.com>
2026-03-25 12:38:23 +0530

    added

---
+++ helo.py
@@ -0,0 +1,21 @@
+s = "ab"
+t = "A"
+ans=s
+k=list(t)
+c=[]
+l=""
+for i in range(len(s)):
+      k=list(t)
+      l=""
+      for j in range(i,len(s)):
+            l+=s[j]
+            if s[j] in k:
+                  k.remove(s[j])
+            if k==[]:
+                  if len(l)<len(ans):
+                        ans=l
+                  break
+if s==l:
+      print("")
+else:
+      print(ans)
